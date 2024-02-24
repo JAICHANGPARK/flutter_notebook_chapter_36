3bf06c236f2611db93d621b05c549941eb494ed0
JAICHANGPARK <aristojeff@gmail.com>
2024-02-24 10:33:09 +0900

    Update my_closet_home_page.dart

--- lib/ep1801_my_closet_app/view/my_closet_home_page.dart
+++ lib/ep1801_my_closet_app/view/my_closet_home_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class MyClosetHomePage extends StatefulWidget {
+  const MyClosetHomePage({super.key});
+
+  @override
+  State<MyClosetHomePage> createState() => _MyClosetHomePageState();
+}
+
+class _MyClosetHomePageState extends State<MyClosetHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
