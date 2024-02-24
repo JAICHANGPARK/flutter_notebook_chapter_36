57d311ea46640288088ac9c038d434353ec6bfe5
JAICHANGPARK <aristojeff@gmail.com>
2024-02-24 10:32:12 +0900

    Update main_1801.dart

--- lib/ep1801_my_closet_app/main_1801.dart
+++ lib/ep1801_my_closet_app/main_1801.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyClosetApp());
+}
+
 class MyClosetApp extends StatelessWidget {
   const MyClosetApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+
+    );
   }
 }
