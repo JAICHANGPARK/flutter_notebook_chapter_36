61de269e943dd67990f9497a4503a8afeeb1bad1
JAICHANGPARK <aristojeff@gmail.com>
2024-02-26 16:55:11 +0900

    Update my_closet_look_page.dart

---
+++ lib/ep1801_my_closet_app/view/my_closet_look_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class MyClosetLookPage extends StatefulWidget {
+  const MyClosetLookPage({super.key});
+
+  @override
+  State<MyClosetLookPage> createState() => _MyClosetLookPageState();
+}
+
+class _MyClosetLookPageState extends State<MyClosetLookPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
