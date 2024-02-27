7d91b33f07159fed64e2980615749ed50c6b3bf6
JAICHANGPARK <aristojeff@gmail.com>
2024-02-27 18:52:27 +0900

    Create closet_look_data.dart

---
+++ lib/ep1801_my_closet_app/data/closet_look_data.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_notebook_chapter_36/ep1801_my_closet_app/model/my_closet_model.dart';
+
+class ClosetLookData {
+  List<ClosetLook> items = [
+    ClosetLook(
+      color: "Pink",
+      size: "S",
+      imgUrl: "",
+      price: "24.99",
+      title: "Relaxed-fit Hoodie",
+    ),
+    ClosetLook(
+      color: "Pink",
+      size: "S",
+      imgUrl: "",
+      price: "24.99",
+      title: "Relaxed-fit Hoodie",
+    ),
+    ClosetLook(
+      color: "Pink",
+      size: "S",
+      imgUrl: "",
+      price: "24.99",
+      title: "Relaxed-fit Hoodie",
+    )
+  ];
+}
