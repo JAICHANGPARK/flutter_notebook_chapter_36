dec2ffb1d03981efa1ab97792365a8ac20b21c6a
JAICHANGPARK <aristojeff@gmail.com>
2024-02-28 18:38:56 +0900

    Update my_closet_rack_page.dart

--- lib/ep1801_my_closet_app/view/my_closet_rack_page.dart
+++ lib/ep1801_my_closet_app/view/my_closet_rack_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
 class MyClosetRackPage extends StatelessWidget {
-  const MyClosetRackPage({Key? key}) : super(key: key);
+  const MyClosetRackPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+
+      ],
+    );
   }
 }
