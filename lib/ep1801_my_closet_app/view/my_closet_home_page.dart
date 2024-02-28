29141f22037e70ca29a24cfdc5e9511930f23549
JAICHANGPARK <aristojeff@gmail.com>
2024-02-28 18:40:21 +0900

    Update my_closet_home_page.dart

--- lib/ep1801_my_closet_app/view/my_closet_home_page.dart
+++ lib/ep1801_my_closet_app/view/my_closet_home_page.dart
@@ -12,6 +12,8 @@ class MyClosetHomePage extends StatefulWidget {
 }
 
 class _MyClosetHomePageState extends State<MyClosetHomePage> {
+
+  int pageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,173 +38,177 @@ class _MyClosetHomePageState extends State<MyClosetHomePage> {
           Positioned.fill(
             child: Column(
               children: [
-                Container(
-                  height: 100,
-                  padding: const EdgeInsets.only(left: 16),
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          color: Colors.white.withOpacity(.2),
-                        ),
-                        padding: const EdgeInsets.all(12),
-                        margin: const EdgeInsets.only(right: 8),
-                        child: const Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CircleAvatar(),
-                            Gap(8),
-                            Text(
-                              "Human",
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            )
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                const Gap(16),
-                Expanded(
-                    child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => const MyClosetLookPage(),
+                Column(
+                  children: [
+                    Container(
+                      height: 100,
+                      padding: const EdgeInsets.only(left: 16),
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.white.withOpacity(.2),
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            margin: const EdgeInsets.only(right: 8),
+                            child: const Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                CircleAvatar(),
+                                Gap(8),
+                                Text(
+                                  "Human",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                )
+                              ],
                             ),
                           );
                         },
-                        child: Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 16),
-                          decoration: BoxDecoration(
-                            color: Colors.white.withOpacity(.2),
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-                                child: Row(
-                                  children: [
-                                    const CircleAvatar(
-                                      radius: 15,
-                                    ),
-                                    const Gap(8),
-                                    const Expanded(
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            "Dream Walker",
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 13,
-                                            ),
+                      ),
+                    ),
+                    const Gap(16),
+                    Expanded(
+                        child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => const MyClosetLookPage(),
+                                ),
+                              );
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 16),
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(.2),
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+                                    child: Row(
+                                      children: [
+                                        const CircleAvatar(
+                                          radius: 15,
+                                        ),
+                                        const Gap(8),
+                                        const Expanded(
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                "Dream Walker",
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 13,
+                                                ),
+                                              ),
+                                              Text(
+                                                "@dreamwalker",
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 12,
+                                                ),
+                                              ),
+                                            ],
                                           ),
-                                          Text(
-                                            "@dreamwalker",
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 12,
-                                            ),
+                                        ),
+                                        IconButton(
+                                          onPressed: () {},
+                                          icon: const Icon(Icons.more_horiz),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  const Divider(
+                                    height: 4,
+                                  ),
+                                  Container(
+                                    height: 340,
+                                    child: const Stack(
+                                      children: [],
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white.withOpacity(.2),
+                                    ),
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 8,
+                                      vertical: 2,
+                                    ),
+                                    child: const Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Icon(
+                                          Icons.shopping_basket,
+                                          size: 16,
+                                          color: Colors.white,
+                                        ),
+                                        Gap(4),
+                                        Text(
+                                          "View items in this look",
+                                          style: TextStyle(
+                                            fontSize: 13,
+                                            color: Colors.white,
                                           ),
-                                        ],
-                                      ),
+                                        ),
+                                      ],
                                     ),
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(Icons.more_horiz),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              const Divider(
-                                height: 4,
-                              ),
-                              Container(
-                                height: 340,
-                                child: const Stack(
-                                  children: [],
-                                ),
-                              ),
-                              Container(
-                                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                                decoration: BoxDecoration(
-                                  color: Colors.white.withOpacity(.2),
-                                ),
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 8,
-                                  vertical: 2,
-                                ),
-                                child: const Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Icon(
-                                      Icons.shopping_basket,
-                                      size: 16,
-                                      color: Colors.white,
-                                    ),
-                                    Gap(4),
-                                    Text(
-                                      "View items in this look",
-                                      style: TextStyle(
-                                        fontSize: 13,
+                                  ),
+                                  const Divider(),
+                                  Row(
+                                    children: [
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          Icons.favorite_border,
+                                        ),
                                         color: Colors.white,
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              const Divider(),
-                              Row(
-                                children: [
-                                  IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Icons.favorite_border,
-                                    ),
-                                    color: Colors.white,
-                                  ),
-                                  IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Icons.chat_bubble_outline,
-                                    ),
-                                    color: Colors.white,
-                                  ),
-                                  IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Icons.send_outlined,
-                                    ),
-                                    color: Colors.white,
-                                  ),
-                                  const Spacer(),
-                                  IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Icons.bookmark_border_outlined,
-                                    ),
-                                    color: Colors.white,
-                                  ),
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          Icons.chat_bubble_outline,
+                                        ),
+                                        color: Colors.white,
+                                      ),
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          Icons.send_outlined,
+                                        ),
+                                        color: Colors.white,
+                                      ),
+                                      const Spacer(),
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          Icons.bookmark_border_outlined,
+                                        ),
+                                        color: Colors.white,
+                                      ),
+                                    ],
+                                  )
                                 ],
-                              )
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ))
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ))
+                  ],
+                ),
               ],
             ),
           ),
