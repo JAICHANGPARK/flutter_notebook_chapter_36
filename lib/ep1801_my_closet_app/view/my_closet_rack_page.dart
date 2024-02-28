7e29156122520784ac6cbbfcbbda433f99bf02bc
JAICHANGPARK <aristojeff@gmail.com>
2024-02-28 18:43:51 +0900

    Update my_closet_rack_page.dart

--- lib/ep1801_my_closet_app/view/my_closet_rack_page.dart
+++ lib/ep1801_my_closet_app/view/my_closet_rack_page.dart
@@ -7,7 +7,50 @@ class MyClosetRackPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-
+        Container(
+          height: 52,
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(.2),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "LOOKS",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    // color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "PRODUCTS",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
       ],
     );
   }
