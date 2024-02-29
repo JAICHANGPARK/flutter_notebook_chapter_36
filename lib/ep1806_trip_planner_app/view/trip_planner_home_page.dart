79d126529f810ae554e2db1874999a7da750bbf7
JAICHANGPARK <aristojeff@gmail.com>
2024-03-01 07:57:09 +0900

    Create trip_planner_home_page.dart

---
+++ lib/ep1806_trip_planner_app/view/trip_planner_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TripPlannerHomePage extends StatefulWidget {
+  const TripPlannerHomePage({super.key});
+
+  @override
+  State<TripPlannerHomePage> createState() => _TripPlannerHomePageState();
+}
+
+class _TripPlannerHomePageState extends State<TripPlannerHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
