aa4a51dc53be352375c620f1e70d530d0d96f13f
JAICHANGPARK <aristojeff@gmail.com>
2024-03-01 07:58:45 +0900

    Update trip_planner_home_page.dart

--- lib/ep1806_trip_planner_app/view/trip_planner_home_page.dart
+++ lib/ep1806_trip_planner_app/view/trip_planner_home_page.dart
@@ -11,6 +11,12 @@ class TripPlannerHomePage extends StatefulWidget {
 class _TripPlannerHomePageState extends State<TripPlannerHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
