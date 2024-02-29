1d36335e13e9f0bc29437947e02ddc7e805afe1d
JAICHANGPARK <aristojeff@gmail.com>
2024-02-29 19:10:24 +0900

    Update trip_planner_login_page.dart

--- lib/ep1806_trip_planner_app/view/trip_planner_login_page.dart
+++ lib/ep1806_trip_planner_app/view/trip_planner_login_page.dart
@@ -6,6 +6,12 @@ class TripPlannerLoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(child: Image.network(src))
+        ],
+      ),
+    );
   }
 }
