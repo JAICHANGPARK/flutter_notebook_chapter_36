f62eb5103dab97a1cbd39524f8515dc384f3a8f3
JAICHANGPARK <aristojeff@gmail.com>
2024-02-29 19:11:03 +0900

    Update trip_planner_login_page.dart

--- lib/ep1806_trip_planner_app/view/trip_planner_login_page.dart
+++ lib/ep1806_trip_planner_app/view/trip_planner_login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TripPlannerLoginPage extends StatelessWidget {
   const TripPlannerLoginPage({super.key});
 
@@ -9,7 +8,12 @@ class TripPlannerLoginPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned.fill(child: Image.network(src))
+          Positioned.fill(
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2023/08/03/22/25/mountain-8168060_1280.jpg",
+              fit: BoxFit.cover,
+            ),
+          )
         ],
       ),
     );
