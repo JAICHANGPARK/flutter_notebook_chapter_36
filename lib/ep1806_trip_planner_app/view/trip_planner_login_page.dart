722e4138e41d3884271006f889589914cffccf6a
JAICHANGPARK <aristojeff@gmail.com>
2024-02-29 19:09:53 +0900

    Create trip_planner_login_page.dart

---
+++ lib/ep1806_trip_planner_app/view/trip_planner_login_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class TripPlannerLoginPage extends StatelessWidget {
+  const TripPlannerLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
