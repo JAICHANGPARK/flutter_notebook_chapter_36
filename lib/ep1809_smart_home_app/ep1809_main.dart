d3e1fb7ba0c896bfa3eee5790daf3eaffda7205e
JAICHANGPARK <aristojeff@gmail.com>
2024-03-03 08:45:26 +0900

    Create ep1809_main.dart

---
+++ lib/ep1809_smart_home_app/ep1809_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+
+class SmartHomeApp extends StatelessWidget {
+  const SmartHomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
