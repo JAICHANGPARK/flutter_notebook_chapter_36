c7e3d976354057c1722af6473a863c43d9f353f0
JAICHANGPARK <aristojeff@gmail.com>
2024-03-02 10:46:54 +0900

    Update trip_planner_home_page.dart

--- lib/ep1806_trip_planner_app/view/trip_planner_home_page.dart
+++ lib/ep1806_trip_planner_app/view/trip_planner_home_page.dart
@@ -156,6 +156,7 @@ class _TripPlannerHomePageState extends State<TripPlannerHomePage> {
                                         image: NetworkImage(
                                           "https://cdn.pixabay.com/photo/2017/02/14/03/03/ama-dablam-2064522_1280.jpg",
                                         ),
+                                        fit: BoxFit.cover,
                                       ),
                                     ),
                                   ),
