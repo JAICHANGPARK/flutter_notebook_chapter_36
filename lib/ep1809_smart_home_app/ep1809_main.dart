0c8e0da43282129eec1714d0e60b03394e1c59d7
JAICHANGPARK <aristojeff@gmail.com>
2024-03-03 08:48:43 +0900

    Update ep1809_main.dart

--- lib/ep1809_smart_home_app/ep1809_main.dart
+++ lib/ep1809_smart_home_app/ep1809_main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'view/smart_home_main_page.dart';
+
 void main() {
   runApp(SmartHomeApp());
 }
@@ -11,7 +13,7 @@ class SmartHomeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: SmartHomeMainPage(),
     );
   }
 }
