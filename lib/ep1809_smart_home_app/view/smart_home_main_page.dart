89cbf195a4a6caca88487a212fe1a9d403cb4816
JAICHANGPARK <aristojeff@gmail.com>
2024-03-06 18:44:55 +0900

    Update smart_home_main_page.dart

--- lib/ep1809_smart_home_app/view/smart_home_main_page.dart
+++ lib/ep1809_smart_home_app/view/smart_home_main_page.dart
@@ -86,13 +86,16 @@ class _SmartHomeMainPageState extends State<SmartHomeMainPage> {
                       },
                       child: Container(
                         decoration: BoxDecoration(
-                            color: tabIndex == 1 ? Colors.pink : Colors.grey[100],
-                            borderRadius: BorderRadius.circular(24),
-                            gradient: LinearGradient(colors: [
+                          color: tabIndex == 1 ? Colors.pink : Colors.grey[100],
+                          borderRadius: BorderRadius.circular(24),
+                          gradient: const LinearGradient(
+                            colors: [
                               Color.fromRGBO(220, 174, 246, 1),
-                              Color.fromRGBO(254  , 181, 226, 1),
-                              Color.fromRGBO(253  , 211, 188, 1),
-                            ])),
+                              Color.fromRGBO(254, 181, 226, 1),
+                              Color.fromRGBO(253, 211, 188, 1),
+                            ],
+                          ),
+                        ),
                         padding: EdgeInsets.symmetric(
                           horizontal: 16,
                         ),
