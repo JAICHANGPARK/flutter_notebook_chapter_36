8978125ecdb256571f7941ed267b0f4d2c611bf0
JAICHANGPARK <aristojeff@gmail.com>
2024-03-07 18:59:09 +0900

    Update video_sharing_home_page.dart

--- lib/ep1814_video_sharing_app/view/video_sharing_home_page.dart
+++ lib/ep1814_video_sharing_app/view/video_sharing_home_page.dart
@@ -15,85 +15,93 @@ class _VideoSharingHomePageState extends State<VideoSharingHomePage> {
         child: Stack(
           children: [
             Positioned.fill(
-                child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.menu),
-                      ),
-                      Text(
-                        "Reelme.",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.menu),
                         ),
-                      ),
-                      Spacer(),
-                      Container(
-                        width: 120,
-                        height: 38,
-                        // color: Colors.grey,
-                        child: Stack(
-                          children: [
-                            Positioned(
-                              child: Transform.rotate(
-                                angle: -0.1,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.red,
-                                    borderRadius: BorderRadius.circular(24),
+                        Text(
+                          "Reelme.",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                        Spacer(),
+                        Container(
+                          width: 120,
+                          height: 38,
+                          // color: Colors.grey,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                child: Transform.rotate(
+                                  angle: -0.1,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.red,
+                                      borderRadius: BorderRadius.circular(24),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            Positioned(
-                              child: Transform.rotate(
-                                angle: 0.1,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.tealAccent,
-                                    borderRadius: BorderRadius.circular(24),
+                              Positioned(
+                                child: Transform.rotate(
+                                  angle: 0.1,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.tealAccent,
+                                      borderRadius: BorderRadius.circular(24),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            Positioned(
-                              child: Container(
-                                // margin: EdgeInsets.symmetric(vertical: 1),
-                                decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.circular(24),
-                                ),
-                                child: Center(
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Icon(Icons.emoji_events_outlined,
-                                      color: Colors.white,),
-                                      Text(
-                                        "TRY PRO",
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white
+                              Positioned(
+                                child: Container(
+                                  // margin: EdgeInsets.symmetric(vertical: 1),
+                                  decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.circular(24),
+                                  ),
+                                  child: Center(
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.emoji_events_outlined,
+                                          color: Colors.white,
                                         ),
-                                      )
-                                    ],
+                                        Text(
+                                          "TRY PRO",
+                                          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+                                        )
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                )
-              ],
-            ))
+                  Container(
+                    height: 100,
+                    child: Placeholder(),
+                  ),
+                  Container(
+                    height: 52,
+                    child: Placeholder(),
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
