18b39c4026a74435b882450438644e1f33b74661
JAICHANGPARK <aristojeff@gmail.com>
2024-03-07 18:46:26 +0900

    Update ep1814_main.dart

--- lib/ep1814_video_sharing_app/ep1814_main.dart
+++ lib/ep1814_video_sharing_app/ep1814_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_36/ep1814_video_sharing_app/view/video_sharing_home_page.dart';
 
 void main() {
   runApp(VideoSharingApp());
@@ -11,7 +12,7 @@ class VideoSharingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: VideoSharingHomePage(),
     );
   }
 }
