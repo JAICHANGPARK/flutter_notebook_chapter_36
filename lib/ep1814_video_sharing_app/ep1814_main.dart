ff925a6ef86485228b87859786a6c731693c6855
JAICHANGPARK <aristojeff@gmail.com>
2024-03-07 18:45:03 +0900

    Update ep1814_main.dart

--- lib/ep1814_video_sharing_app/ep1814_main.dart
+++ lib/ep1814_video_sharing_app/ep1814_main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(app);
+}
+
+
+class VideoSharingApp extends StatelessWidget {
+  const VideoSharingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
