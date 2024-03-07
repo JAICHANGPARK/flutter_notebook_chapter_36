922016ed6d39267e9edf01112e4a01fa6996b887
JAICHANGPARK <aristojeff@gmail.com>
2024-03-07 18:46:14 +0900

    Update video_sharing_home_page.dart

--- lib/ep1814_video_sharing_app/view/video_sharing_home_page.dart
+++ lib/ep1814_video_sharing_app/view/video_sharing_home_page.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+
+class VideoSharingHomePage extends StatefulWidget {
+  const VideoSharingHomePage({super.key});
+
+  @override
+  State<VideoSharingHomePage> createState() => _VideoSharingHomePageState();
+}
+
+class _VideoSharingHomePageState extends State<VideoSharingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
