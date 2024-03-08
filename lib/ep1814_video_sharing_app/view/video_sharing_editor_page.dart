2e13fe191fa4b7b4bf1d1868d0e0d8ed30281cc3
JAICHANGPARK <aristojeff@gmail.com>
2024-03-08 11:51:17 +0900

    Update video_sharing_editor_page.dart

--- lib/ep1814_video_sharing_app/view/video_sharing_editor_page.dart
+++ lib/ep1814_video_sharing_app/view/video_sharing_editor_page.dart
@@ -1,1 +1,21 @@
 import 'package:flutter/material.dart';
+
+class VideoSharingEditorPage extends StatefulWidget {
+  const VideoSharingEditorPage({super.key});
+
+  @override
+  State<VideoSharingEditorPage> createState() => _VideoSharingEditorPageState();
+}
+
+class _VideoSharingEditorPageState extends State<VideoSharingEditorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
