8302a194bd45012bc3a392aa6321e2e714a57812
JAICHANGPARK <aristojeff@gmail.com>
2024-03-11 17:15:15 +0900

    Create yoga_home_page.dart

---
+++ lib/ep1817_yoga_app/view/yoga_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class YogaHomePage extends StatefulWidget {
+  const YogaHomePage({super.key});
+
+  @override
+  State<YogaHomePage> createState() => _YogaHomePageState();
+}
+
+class _YogaHomePageState extends State<YogaHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+
+  }
+}
