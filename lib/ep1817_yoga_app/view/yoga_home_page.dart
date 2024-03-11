69307ada86312c709e9b56594fb91f5c49c0a98b
JAICHANGPARK <aristojeff@gmail.com>
2024-03-11 17:19:32 +0900

    Update yoga_home_page.dart

--- lib/ep1817_yoga_app/view/yoga_home_page.dart
+++ lib/ep1817_yoga_app/view/yoga_home_page.dart
@@ -11,46 +11,53 @@ class _YogaHomePageState extends State<YogaHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                height: 64,
-                width: 42,
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                ),
-                child: IconButton(
-                  icon: Icon(
-                    Icons.menu,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Container(
+                    height: 64,
+                    width: 48,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.menu,
+                      ),
+                      onPressed: () {},
+                    ),
                   ),
-                  onPressed: () {},
-                ),
-              ),
-              Expanded(
-                child: Center(
-                  child: Text(
-                    "Yoga",
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        "Yoga",
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                height: 64,
-                width: 42,
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(117, 118, 88, 1),
-                ),
-                child: IconButton(
-                  icon: Icon(
-                    Icons.search,
+                  Container(
+                    height: 64,
+                    width: 48,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.search,
+                      ),
+                      onPressed: () {},
+                    ),
                   ),
-                  onPressed: () {},
-                ),
+                ],
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
