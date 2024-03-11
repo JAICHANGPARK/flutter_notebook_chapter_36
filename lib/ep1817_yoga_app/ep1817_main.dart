d67965637b9d18659fc3347905c4314763fa32c2
JAICHANGPARK <aristojeff@gmail.com>
2024-03-11 17:13:48 +0900

    Update ep1817_main.dart

---
+++ lib/ep1817_yoga_app/ep1817_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(YogaApp());
+}
+
+class YogaApp extends StatelessWidget {
+  const YogaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
