ef816ba8e89161d11725bbba75a864e6b564f46c
JAICHANGPARK <aristojeff@gmail.com>
2024-03-14 18:54:21 +0900

    Create yoga_detail_page.dart

---
+++ lib/ep1817_yoga_app/view/yoga_detail_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class YogaDetailsPage extends StatefulWidget {
+  const YogaDetailsPage({super.key});
+
+  @override
+  State<YogaDetailsPage> createState() => _YogaDetailsPageState();
+}
+
+class _YogaDetailsPageState extends State<YogaDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
