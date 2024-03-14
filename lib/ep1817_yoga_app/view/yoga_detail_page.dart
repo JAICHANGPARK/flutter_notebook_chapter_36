5b5a9ad3b4c6d8356e13093a80d4d4a58988833b
JAICHANGPARK <aristojeff@gmail.com>
2024-03-14 19:07:16 +0900

    Update yoga_detail_page.dart

--- lib/ep1817_yoga_app/view/yoga_detail_page.dart
+++ lib/ep1817_yoga_app/view/yoga_detail_page.dart
@@ -241,7 +241,20 @@ class _YogaDetailsPageState extends State<YogaDetailsPage> {
                   ),
                 ],
               ),
-            )
+            ),
+            Text("Description"),
+            Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              maxLines: 4,
+            ),
+            Text("Select Your Tutor"),
+            SizedBox(height: 64,child: Placeholder(),),
+            SizedBox(height: 82,
+            child: Row(
+              children: [
+                Expanded(child: Placeholder()),
+              ],
+            ),)
           ],
         ),
       ),
