ce0ca915a28f3a2230da92d972fb1402f26ff9f5
JAICHANGPARK <aristojeff@gmail.com>
2024-03-14 19:04:32 +0900

    Update yoga_detail_page.dart

--- lib/ep1817_yoga_app/view/yoga_detail_page.dart
+++ lib/ep1817_yoga_app/view/yoga_detail_page.dart
@@ -154,30 +154,93 @@ class _YogaDetailsPageState extends State<YogaDetailsPage> {
                 ],
               ),
             ),
-            Row(
-              children: [
-                Container(
-                  height: 64,
-                  width: 48,
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(117, 118, 88, 1),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.play_arrow,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 64,
+                          width: 48,
+                          decoration: BoxDecoration(
+                            color: Colors.red[50],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.bar_chart,
+                            ),
+                            onPressed: () {},
+                            color: Colors.white,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Text("Level"),
+                            Text("1"),
+                          ],
+                        )
+                      ],
                     ),
-                    onPressed: () {},
-                    color: Colors.white,
                   ),
-                ),
-                Column(
-                  children: [
-                    Text("Level"),
-                    Text("1"),
-                  ],
-                )
-              ],
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 64,
+                          width: 48,
+                          decoration: BoxDecoration(
+                            color: Colors.red[50],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.bar_chart,
+                            ),
+                            onPressed: () {},
+                            color: Colors.white,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Text("Level"),
+                            Text("1"),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 64,
+                          width: 48,
+                          decoration: BoxDecoration(
+                            color: Colors.red[50],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.bar_chart,
+                            ),
+                            onPressed: () {},
+                            color: Colors.white,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Text("Level"),
+                            Text("1"),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             )
           ],
         ),
