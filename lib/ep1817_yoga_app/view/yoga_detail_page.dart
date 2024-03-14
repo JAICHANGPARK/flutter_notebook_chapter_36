33ec36beba8b4184e3000a2fe25016fc2fbfd32f
JAICHANGPARK <aristojeff@gmail.com>
2024-03-14 18:56:17 +0900

    Update yoga_detail_page.dart

--- lib/ep1817_yoga_app/view/yoga_detail_page.dart
+++ lib/ep1817_yoga_app/view/yoga_detail_page.dart
@@ -14,11 +14,52 @@ class _YogaDetailsPageState extends State<YogaDetailsPage> {
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              children: [
-
-              ],
-            )
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Container(
+                    height: 64,
+                    width: 48,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.arrow_back,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        "Details",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 64,
+                    width: 48,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.favorite_border,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
 
           ],
         ),
