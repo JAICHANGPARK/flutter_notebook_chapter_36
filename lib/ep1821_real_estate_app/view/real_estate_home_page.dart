3e09f5146b700a7274bb7bd8ca4e6a3699421d20
JAICHANGPARK <aristojeff@gmail.com>
2024-03-15 17:57:35 +0900

    Update real_estate_home_page.dart

--- lib/ep1821_real_estate_app/view/real_estate_home_page.dart
+++ lib/ep1821_real_estate_app/view/real_estate_home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RealEstateHomePage extends StatefulWidget {
@@ -14,32 +15,66 @@ class _RealEstateHomePageState extends State<RealEstateHomePage> {
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              children: [
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.grey[300]!,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey[300]!,
+                      ),
+                    ),
+                    child: Text(
+                      "HOMECOCO",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 26,
+                      ),
                     ),
                   ),
-                  child: Text(
-                    "HOMECOCO",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w900,
-                      fontSize: 26,
+                  Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.notifications_none),
+                  ),
+                  CircleAvatar(
+                    radius: 27,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200]!,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(24),
+                  bottomLeft: Radius.circular(24),
+                ),
+              ),
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.search),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text("Find Your New"),
+                        Text("apartment, House, lands and more"),
+                      ],
                     ),
                   ),
-                ),
-                Spacer(),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.notifications_none),
-                ),
-                CircleAvatar(
-                  radius: 27,
-                ),
-              ],
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.filter_list_outlined,
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
