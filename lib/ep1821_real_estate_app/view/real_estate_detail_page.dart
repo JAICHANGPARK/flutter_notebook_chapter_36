74b7cfc75ba44ba90d8870d482b9b3b95030b559
JAICHANGPARK <aristojeff@gmail.com>
2024-03-17 09:23:11 +0900

    Update real_estate_detail_page.dart

--- lib/ep1821_real_estate_app/view/real_estate_detail_page.dart
+++ lib/ep1821_real_estate_app/view/real_estate_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class RealEstateDetailPage extends StatefulWidget {
   const RealEstateDetailPage({super.key});
@@ -25,16 +26,23 @@ class _RealEstateDetailPageState extends State<RealEstateDetailPage> {
                 border: Border.all(),
                 borderRadius: BorderRadius.circular(32),
               ),
+              child: Center(
+                child: Text("Explore in VR"),
+              ),
             )),
+            Gap(12),
             Expanded(
-              
                 child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(),
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(32),
+              decoration: BoxDecoration(
+                border: Border.all(),
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(32),
+              ),
+                  child: Center(
+                    child: Text("Book a Call"
+                        ),
                   ),
-                ))
+            ))
           ],
         ),
       ),
