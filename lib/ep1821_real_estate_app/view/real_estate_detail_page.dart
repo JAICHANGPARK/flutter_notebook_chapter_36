a7547c8fe99c3df86b81acec89ad2c0aafeb48d0
JAICHANGPARK <aristojeff@gmail.com>
2024-03-17 09:20:49 +0900

    Create real_estate_detail_page.dart

---
+++ lib/ep1821_real_estate_app/view/real_estate_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class RealEstateDetailPage extends StatefulWidget {
+  const RealEstateDetailPage({super.key});
+
+  @override
+  State<RealEstateDetailPage> createState() => _RealEstateDetailPageState();
+}
+
+class _RealEstateDetailPageState extends State<RealEstateDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
