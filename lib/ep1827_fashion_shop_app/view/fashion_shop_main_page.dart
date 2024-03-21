81197cf1fdf6cbf44b1bb4149ee597afd6ebc2bd
JAICHANGPARK <aristojeff@gmail.com>
2024-03-21 19:16:10 +0900

    Create fashion_shop_main_page.dart

---
+++ lib/ep1827_fashion_shop_app/view/fashion_shop_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FashionShopMainPage extends StatefulWidget {
+  const FashionShopMainPage({super.key});
+
+  @override
+  State<FashionShopMainPage> createState() => _FashionShopMainPageState();
+}
+
+class _FashionShopMainPageState extends State<FashionShopMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
