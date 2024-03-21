4994671e0a2f5c365a1e3d712bcd0c8ceddc70ba
JAICHANGPARK <aristojeff@gmail.com>
2024-03-21 19:16:26 +0900

    Update ep1827_main.dart

--- lib/ep1827_fashion_shop_app/ep1827_main.dart
+++ lib/ep1827_fashion_shop_app/ep1827_main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'view/fashion_shop_main_page.dart';
+
 void main() {
   runApp(FashionShopApp());
 }
@@ -10,7 +12,7 @@ class FashionShopApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ,
+      home: FashionShopMainPage(),
     );
   }
 }
