1de81e50dec1c1e2a3b49f37f4d6b4bbec06eef3
JAICHANGPARK <aristojeff@gmail.com>
2024-03-24 00:14:39 +0900

    Create fashion_shop_cart_widget.dart

---
+++ lib/ep1827_fashion_shop_app/widget/fashion_shop_cart_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FashionShopCartWidget extends StatefulWidget {
+  const FashionShopCartWidget({super.key});
+
+  @override
+  State<FashionShopCartWidget> createState() => _FashionShopCartWidgetState();
+}
+
+class _FashionShopCartWidgetState extends State<FashionShopCartWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
