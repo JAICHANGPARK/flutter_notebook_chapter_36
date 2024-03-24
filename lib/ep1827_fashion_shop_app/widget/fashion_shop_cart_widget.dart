ce24a379900808f0f4e0f4ecd96ab67db359f48a
JAICHANGPARK <aristojeff@gmail.com>
2024-03-24 11:25:19 +0900

    Update fashion_shop_cart_widget.dart

--- lib/ep1827_fashion_shop_app/widget/fashion_shop_cart_widget.dart
+++ lib/ep1827_fashion_shop_app/widget/fashion_shop_cart_widget.dart
@@ -69,141 +69,159 @@ class _FashionShopCartWidgetState extends State<FashionShopCartWidget> {
         centerTitle: true,
         title: Text("CART"),
       ),
-      body: Column(
-        children: [
-          Divider(),
-          ...carts.map(
-            (e) => Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Row(
-                    children: [
-                      Container(
-                        height: 82,
-                        width: 82,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 16,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Divider(),
+            ...carts.map(
+              (e) => Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 82,
+                          width: 82,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
                           ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(e.title ?? "?"),
-                                      Text(e.subtitle ?? "??"),
-                                    ],
-                                  ),
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      color: Colors.grey[200]!,
-                                    ),
-                                    child: Row(
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
                                       children: [
-                                        Icon(Icons.remove_circle_outline),
-                                        Gap(4),
-                                        Text("${e.count}"),
-                                        Gap(4),
-                                        Icon(
-                                          Icons.add_circle,
-                                        ),
+                                        Text(e.title ?? "?"),
+                                        Text(e.subtitle ?? "??"),
                                       ],
                                     ),
-                                  )
-                                ],
-                              ),
-                              Gap(16),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text("\$${e.price * e.count}"),
-                                  Text(
-                                    "REMOVE",
-                                    style: TextStyle(
-                                      decoration: TextDecoration.underline,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ],
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.grey[200]!,
+                                      ),
+                                      child: Row(
+                                        children: [
+                                          Icon(Icons.remove_circle_outline),
+                                          Gap(4),
+                                          Text("${e.count}"),
+                                          Gap(4),
+                                          Icon(
+                                            Icons.add_circle,
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Gap(16),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text("\$${e.price * e.count}"),
+                                    Text(
+                                      "REMOVE",
+                                      style: TextStyle(
+                                        decoration: TextDecoration.underline,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                ],
+              ),
+            ),
+            Container(
+              height: 120,
+              margin: EdgeInsets.all(12),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.grey[200]!,
                       ),
+                    ),
+                  )),
+                  Gap(12),
+                  Expanded(
+                      child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.grey[200]!,
+                      ),
+                    ),
+                  )),
+                ],
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text("Subtotal:"),
+                      Text("\$"),
                     ],
                   ),
-                ),
-                Divider(),
-              ],
-            ),
-          ),
-          Container(
-            height: 120,
-            margin: EdgeInsets.all(12),
-            child: Row(
-              children: [
-                Expanded(
-                    child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(
-                      color: Colors.grey[200]!,
-                    ),
+                  Gap(12),
+                  Row(
+                    children: [
+                      Text("Delivery Fee:"),
+                      Text("\$"),
+                    ],
                   ),
-                )),
-                Gap(12),
-                Expanded(
-                    child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(
-                      color: Colors.grey[200]!,
-                    ),
+                  Divider(
+                    height: 32,
                   ),
-                )),
-              ],
+                  Row(
+                    children: [
+                      Text("Delivery Fee:"),
+                      Text("\$"),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          Divider(),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Text("Subtotal:"),
-                    Text("\$"),
-                  ],
+            Container(
+              height: 48,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              margin: EdgeInsets.all(12),
+              child: Center(
+                child: Text(
+                  "CHECKOUT",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
-                Gap(12),
-                Row(
-                  children: [
-                    Text("Delivery Fee:"),
-                    Text("\$"),
-                  ],
-                ),
-
-                Divider(height: 32,),
-                Row(
-                  children: [
-                    Text("Delivery Fee:"),
-                    Text("\$"),
-                  ],
-                ),
-              ],
-            ),
-          )
-
-        ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
