f61be50cb1972c1a4232d8394ce547d3083014cc
JAICHANGPARK <aristojeff@gmail.com>
2024-03-24 11:20:10 +0900

    Update fashion_shop_cart_widget.dart

--- lib/ep1827_fashion_shop_app/widget/fashion_shop_cart_widget.dart
+++ lib/ep1827_fashion_shop_app/widget/fashion_shop_cart_widget.dart
@@ -38,6 +38,13 @@ class _FashionShopCartWidgetState extends State<FashionShopCartWidget> {
         price: 1270,
       ),
     );
+    carts.add(
+      ShopCart(
+        title: "Brushed Leather",
+        subtitle: "Black/LP/37",
+        price: 1270,
+      ),
+    );
   }
 
   @override
@@ -65,63 +72,90 @@ class _FashionShopCartWidgetState extends State<FashionShopCartWidget> {
                         ),
                       ),
                       Expanded(
-                          child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 16,
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(e.title ?? "?"),
-                                    Text(e.subtitle ?? "??"),
-                                  ],
-                                ),
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey[200]!,
-                                  ),
-                                  child: Row(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
                                     children: [
-                                      Icon(Icons.remove_circle_outline),
-                                      Gap(4),
-                                      Text("${e.count}"),
-                                      Gap(4),
-                                      Icon(
-                                        Icons.add_circle,
-                                      ),
+                                      Text(e.title ?? "?"),
+                                      Text(e.subtitle ?? "??"),
                                     ],
                                   ),
-                                )
-                              ],
-                            ),
-                            Gap(16),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text("\$${e.price * e.count}"),
-                                Text(
-                                  "REMOVE",
-                                  style: TextStyle(
-                                    decoration: TextDecoration.underline,
-                                  ),
-                                )
-                              ],
-                            ),
-                          ],
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[200]!,
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Icon(Icons.remove_circle_outline),
+                                        Gap(4),
+                                        Text("${e.count}"),
+                                        Gap(4),
+                                        Icon(
+                                          Icons.add_circle,
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Gap(16),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text("\$${e.price * e.count}"),
+                                  Text(
+                                    "REMOVE",
+                                    style: TextStyle(
+                                      decoration: TextDecoration.underline,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                      ))
+                      ),
                     ],
                   ),
-                )
+                ),
+                Divider(),
               ],
             ),
           ),
+          SizedBox(
+            height: 120,
+            child: Row(
+              children: [
+                Expanded(
+                    child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      color: Colors.grey[200]!,
+                    ),
+                  ),
+                )),
+                Expanded(
+                    child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      color: Colors.grey[200]!,
+                    ),
+                  ),
+                )),
+              ],
+            ),
+          )
         ],
       ),
     );
