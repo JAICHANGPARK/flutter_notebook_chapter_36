8880d4ff0e6c647d4c32e544030ea6f17fc3683f
JAICHANGPARK <aristojeff@gmail.com>
2024-03-25 17:46:31 +0900

    Update chat_bot_login_page.dart

--- lib/ep1831_chat_bot_app/views/chat_bot_login_page.dart
+++ lib/ep1831_chat_bot_app/views/chat_bot_login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class ChatBotLoginPage extends StatefulWidget {
   const ChatBotLoginPage({super.key});
@@ -35,12 +36,21 @@ class _ChatBotLoginPageState extends State<ChatBotLoginPage> {
                     fontSize: 24,
                   ),
                 ),
+                Gap(16),
                 Text(
                   "Unlock meaningful connections through AI-driven conversations tailoared to your needs",
                   style: TextStyle(
                     color: Colors.white,
                   ),
                   textAlign: TextAlign.center,
+                ),
+                Gap(42),
+                Container(
+                  height: 42,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    
+                  ),
                 )
               ],
             ),
