0f4733318d0ba52692d453b0ed0e3f8cc0a37807
JAICHANGPARK <aristojeff@gmail.com>
2024-03-25 17:40:34 +0900

    Update ep1831_main.dart

--- lib/ep1831_chat_bot_app/ep1831_main.dart
+++ lib/ep1831_chat_bot_app/ep1831_main.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  
+}
+
+class AiChatBotApp extends StatelessWidget {
+  const AiChatBotApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
