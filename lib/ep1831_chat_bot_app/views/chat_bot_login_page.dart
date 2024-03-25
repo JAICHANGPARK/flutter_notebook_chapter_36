2025cfd90ae4fec03203cf32f8b647771a583033
JAICHANGPARK <aristojeff@gmail.com>
2024-03-25 17:41:58 +0900

    Create chat_bot_login_page.dart

---
+++ lib/ep1831_chat_bot_app/views/chat_bot_login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatBotLoginPage extends StatefulWidget {
+  const ChatBotLoginPage({super.key});
+
+  @override
+  State<ChatBotLoginPage> createState() => _ChatBotLoginPageState();
+}
+
+class _ChatBotLoginPageState extends State<ChatBotLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
