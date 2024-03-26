58139a6f65931902296cff5058d6d24f266fce74
JAICHANGPARK <aristojeff@gmail.com>
2024-03-26 18:27:23 +0900

    Update chat_bot_home_page.dart

---
+++ lib/ep1831_chat_bot_app/views/chat_bot_home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChatBotHomePage extends StatefulWidget {
+  const ChatBotHomePage({super.key});
+
+  @override
+  State<ChatBotHomePage> createState() => _ChatBotHomePageState();
+}
+
+class _ChatBotHomePageState extends State<ChatBotHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
