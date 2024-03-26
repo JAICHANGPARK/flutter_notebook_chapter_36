4fb19d5a08a4371255239910c20cab41a0451f7f
JAICHANGPARK <aristojeff@gmail.com>
2024-03-26 18:32:29 +0900

    Update chat_bot_home_page.dart

--- lib/ep1831_chat_bot_app/views/chat_bot_home_page.dart
+++ lib/ep1831_chat_bot_app/views/chat_bot_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class ChatBotHomePage extends StatefulWidget {
   const ChatBotHomePage({super.key});
@@ -14,10 +15,12 @@ class _ChatBotHomePageState extends State<ChatBotHomePage> {
       backgroundColor: Colors.black,
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding: const EdgeInsets.all(12.0),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
                     children: [
@@ -26,17 +29,32 @@ class _ChatBotHomePageState extends State<ChatBotHomePage> {
                           Icons.menu,
                         ),
                       ),
+                      Gap(12),
                       Expanded(
                           child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text("Wednesday."),
-                          Text("13 March 2024"),
+                          Text(
+                            "Wednesday.",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          Gap(4),
+                          Text(
+                            "13 March 2024",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
                         ],
                       )),
                       CircleAvatar(),
                     ],
                   ),
+                  Gap(24),
                   Text(
                     "Good Morning Dream",
                     style: TextStyle(
@@ -50,7 +68,6 @@ class _ChatBotHomePageState extends State<ChatBotHomePage> {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-
                   Container(
                     margin: EdgeInsets.symmetric(vertical: 16),
                     height: 62,
