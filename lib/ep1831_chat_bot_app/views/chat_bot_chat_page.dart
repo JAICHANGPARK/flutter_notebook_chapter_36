8b5b1808f7157ce07345057e80b10652abbcef1f
JAICHANGPARK <aristojeff@gmail.com>
2024-03-28 19:28:42 +0900

    Update chat_bot_chat_page.dart

--- lib/ep1831_chat_bot_app/views/chat_bot_chat_page.dart
+++ lib/ep1831_chat_bot_app/views/chat_bot_chat_page.dart
@@ -1,2 +1,33 @@
 import 'package:flutter/material.dart';
 
+class ChatBotChatPage extends StatefulWidget {
+  const ChatBotChatPage({super.key});
+
+  @override
+  State<ChatBotChatPage> createState() => _ChatBotChatPageState();
+}
+
+class _ChatBotChatPageState extends State<ChatBotChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(),
+                Expanded(
+                  child: Center(
+                    child: Text("The Best Foot..."),
+                  ),
+                ),
+                CircleAvatar(),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
