42b3d6c411ff4098dd8cd58edc11ee21fb2e29c5
JAICHANGPARK <aristojeff@gmail.com>
2024-03-30 09:38:13 +0900

    Update ep1836_main.dart

--- lib/ep1836_real_estate_app/ep1836_main.dart
+++ lib/ep1836_real_estate_app/ep1836_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'views/real_estate_2_start_page.dart';
 
@@ -12,6 +13,9 @@ class RealEstateApp2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: GoogleFonts.montserrat().fontFamily,
+      ),
       home: RealEstate2StartPage(),
     );
   }
