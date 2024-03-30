3e8db579bd4210cdf425cdb3aff8a074a9d768c0
JAICHANGPARK <aristojeff@gmail.com>
2024-03-30 09:26:51 +0900

    Update ep1836_main.dart

---
+++ lib/ep1836_real_estate_app/ep1836_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class RealEstateApp2 extends StatelessWidget {
+  const RealEstateApp2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
