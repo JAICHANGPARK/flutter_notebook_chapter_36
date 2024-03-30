0d1785c846a7e2b99bc60d83325127b0bd86db39
JAICHANGPARK <aristojeff@gmail.com>
2024-03-30 09:28:03 +0900

    Create real_estate_2_start_page.dart

---
+++ lib/ep1836_real_estate_app/views/real_estate_2_start_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class RealEstate2StartPage extends StatefulWidget {
+  const RealEstate2StartPage({super.key});
+
+  @override
+  State<RealEstate2StartPage> createState() => _RealEstate2StartPageState();
+}
+
+class _RealEstate2StartPageState extends State<RealEstate2StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
