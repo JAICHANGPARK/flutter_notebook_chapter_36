45bbd811ca8105deff18b5fe62a8214435bf0335
JAICHANGPARK <aristojeff@gmail.com>
2024-03-30 09:31:52 +0900

    Update real_estate_2_start_page.dart

--- lib/ep1836_real_estate_app/views/real_estate_2_start_page.dart
+++ lib/ep1836_real_estate_app/views/real_estate_2_start_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 class RealEstate2StartPage extends StatefulWidget {
   const RealEstate2StartPage({super.key});
 
@@ -15,14 +14,20 @@ class _RealEstate2StartPageState extends State<RealEstate2StartPage> {
     return Scaffold(
       body: Stack(
         children: [
+          Positioned.fill(
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2020/09/01/15/03/building-5535464_1280.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
           Positioned(
             left: 24,
             right: 24,
             top: 64,
             bottom: 32,
-            child: Column(children: [
-
-            ],),
+            child: Column(
+              children: [],
+            ),
           )
         ],
       ),
