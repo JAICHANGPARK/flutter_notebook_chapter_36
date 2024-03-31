0516c8bbc2b8fb63644ab2f42e09278148be07c9
JAICHANGPARK <aristojeff@gmail.com>
2024-03-31 09:09:58 +0900

    Update real_estate_2_home_page.dart

---
+++ lib/ep1836_real_estate_app/views/real_estate_2_home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class RealEstate2HomePage extends StatefulWidget {
+  const RealEstate2HomePage({super.key});
+
+  @override
+  State<RealEstate2HomePage> createState() => _RealEstate2HomePageState();
+}
+
+class _RealEstate2HomePageState extends State<RealEstate2HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
