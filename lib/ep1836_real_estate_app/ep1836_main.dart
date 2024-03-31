b49a49e2fce244aeaf702943e28861b4bfcd6369
JAICHANGPARK <aristojeff@gmail.com>
2024-03-31 09:10:54 +0900

    Update ep1836_main.dart

--- lib/ep1836_real_estate_app/ep1836_main.dart
+++ lib/ep1836_real_estate_app/ep1836_main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_36/ep1821_real_estate_app/view/real_estate_home_page.dart';
+import 'package:flutter_notebook_chapter_36/ep1836_real_estate_app/views/real_estate_2_home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'views/real_estate_2_start_page.dart';
@@ -16,7 +18,11 @@ class RealEstateApp2 extends StatelessWidget {
       theme: ThemeData(
         fontFamily: GoogleFonts.montserrat().fontFamily,
       ),
-      home: RealEstate2StartPage(),
+      routes: {
+        "/start": (context) => RealEstate2StartPage(),
+        "/": (context) => RealEstate2HomePage(),
+      },
+     
     );
   }
 }
