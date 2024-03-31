5191ed6bf6c0f9ce743a939923c07d699b11cfad
JAICHANGPARK <aristojeff@gmail.com>
2024-03-31 09:14:03 +0900

    Update real_estate_2_home_page.dart

--- lib/ep1836_real_estate_app/views/real_estate_2_home_page.dart
+++ lib/ep1836_real_estate_app/views/real_estate_2_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class RealEstate2HomePage extends StatefulWidget {
   const RealEstate2HomePage({super.key});
 
@@ -12,11 +11,44 @@ class _RealEstate2HomePageState extends State<RealEstate2HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      // backgroundColor: Colors.black,
       body: SafeArea(
         child: Stack(
           children: [
-
+            Positioned.fill(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [],
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        return Container();
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+                bottom: 24,
+                left: 24,
+                right: 24,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                  ),
+                  child: Row(
+                    children: [
+                      CircleAvatar(),
+                      CircleAvatar(),
+                      CircleAvatar(),
+                      CircleAvatar(),
+                    ],
+                  ),
+                ))
           ],
         ),
       ),
