7af84b64bd4832a1651587345c047c632ee494df
JAICHANGPARK <aristojeff@gmail.com>
2024-04-01 17:52:50 +0900

    Update real_estate_2_home_page.dart

--- lib/ep1836_real_estate_app/views/real_estate_2_home_page.dart
+++ lib/ep1836_real_estate_app/views/real_estate_2_home_page.dart
@@ -219,6 +219,10 @@ class _RealEstate2HomePageState extends State<RealEstate2HomePage> {
                           return Container(
                             height: 320,
                             margin: EdgeInsets.only(bottom: 16),
+                            decoration: BoxDecoration(
+                              color: Colors.purple,
+                              borderRadius: BorderRadius.circular(16)
+                            ),
                             child: Placeholder(),
                           );
                         },
