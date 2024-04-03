d9c6d8f113595c575f6bc622b11c7f3ef4661a7e
JAICHANGPARK <aristojeff@gmail.com>
2024-04-03 19:01:48 +0900

    Update real_estate_2_detail_page.dart

--- lib/ep1836_real_estate_app/views/real_estate_2_detail_page.dart
+++ lib/ep1836_real_estate_app/views/real_estate_2_detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RealEstate2DetailPage extends StatefulWidget {
@@ -11,17 +12,47 @@ class _RealEstate2DetailPageState extends State<RealEstate2DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Stack(
         children: [
-          Column(
-            children: [],
+          Positioned.fill(
+            child: Column(
+              children: [],
+            ),
           ),
           Positioned(
+            left: 24,
+            right: 24,
+            bottom: 32,
             child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(34, 34, 34, 1),
+              ),
+              padding: EdgeInsets.all(8),
+              height: 72,
               child: Row(
                 children: [
                   Text("\$1.200"),
                   Text(" / Night"),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xff775cff),
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 24,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Book Now",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
