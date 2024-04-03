438b275e13b9ee48122f664dc15c0a3179986c74
JAICHANGPARK <aristojeff@gmail.com>
2024-04-03 18:55:27 +0900

    Update ep1836_main.dart

--- lib/ep1836_real_estate_app/ep1836_main.dart
+++ lib/ep1836_real_estate_app/ep1836_main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_36/ep1821_real_estate_app/view/real_estate_home_page.dart';
 import 'package:flutter_notebook_chapter_36/ep1836_real_estate_app/views/real_estate_2_home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'views/real_estate_2_detail_page.dart';
 import 'views/real_estate_2_start_page.dart';
 
 void main() {
@@ -22,6 +22,7 @@ class RealEstateApp2 extends StatelessWidget {
       routes: {
         "/": (context) => RealEstate2HomePage(),
         "/start": (context) => RealEstate2StartPage(),
+        "/detail": (context) => RealEstate2DetailPage(),
       },
     );
   }
