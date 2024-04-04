4676c02e578127522947c55cdb8337db58ae6b92
JAICHANGPARK <aristojeff@gmail.com>
2024-04-04 19:22:36 +0900

    Create ai_travel_home_page.dart

---
+++ lib/ep1841_ai_travel_app/views/ai_travel_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AiTravelHomePage extends StatefulWidget {
+  const AiTravelHomePage({super.key});
+
+  @override
+  State<AiTravelHomePage> createState() => _AiTravelHomePageState();
+}
+
+class _AiTravelHomePageState extends State<AiTravelHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
