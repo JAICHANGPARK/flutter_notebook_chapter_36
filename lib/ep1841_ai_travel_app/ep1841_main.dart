110ee8ed9ee30250fcaf6cf2bf89a91e0ca0a36e
JAICHANGPARK <aristojeff@gmail.com>
2024-04-04 19:20:31 +0900

    Update ep1841_main.dart

---
+++ lib/ep1841_ai_travel_app/ep1841_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+
+}
+
+class AITravelApp extends StatelessWidget {
+  const AITravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
