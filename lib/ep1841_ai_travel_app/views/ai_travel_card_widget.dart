fc75fd78823da60bee934753c3ab57fee6d5124a
JAICHANGPARK <aristojeff@gmail.com>
2024-04-07 16:11:13 +0900

    Create ai_travel_card_widget.dart

---
+++ lib/ep1841_ai_travel_app/views/ai_travel_card_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class AITravelCardWidget extends StatelessWidget {
+  const AITravelCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
