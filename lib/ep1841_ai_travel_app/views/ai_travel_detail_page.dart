eb75c5fba02018b868a1454349b54b5e30a5ded5
JAICHANGPARK <aristojeff@gmail.com>
2024-04-08 17:23:54 +0900

    Update ai_travel_detail_page.dart

--- lib/ep1841_ai_travel_app/views/ai_travel_detail_page.dart
+++ lib/ep1841_ai_travel_app/views/ai_travel_detail_page.dart
@@ -14,7 +14,48 @@ class _AiTravelDetailPageState extends State<AiTravelDetailPage> {
       body: Column(
         children: [
           Container(
-            height: 320,
+            height: 360,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              image: DecorationImage(
+                image: NetworkImage(
+                  "https://cdn.pixabay.com/photo/2022/10/01/01/55/gyeongbokgung-palace-7490491_1280.jpg",
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 72,
+                    left: 16,
+                    right: 16,
+                  ),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        child: Icon(
+                          Icons.arrow_back_ios_new,
+                        ),
+                      ),
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            "Detail Destination",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      CircleAvatar(),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
           Expanded(
             child: Container(
