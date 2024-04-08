be36141688f0ff254cd6d355258d82ac6cf28771
JAICHANGPARK <aristojeff@gmail.com>
2024-04-08 17:18:14 +0900

    Create ai_travel_detail_page.dart

---
+++ lib/ep1841_ai_travel_app/views/ai_travel_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AiTravelDetailPage extends StatefulWidget {
+  const AiTravelDetailPage({super.key});
+
+  @override
+  State<AiTravelDetailPage> createState() => _AiTravelDetailPageState();
+}
+
+class _AiTravelDetailPageState extends State<AiTravelDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
