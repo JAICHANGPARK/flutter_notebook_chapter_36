963a3d66c915245b95d1aa77277205fbdbe82a95
JAICHANGPARK <aristojeff@gmail.com>
2024-04-10 08:35:32 +0900

    Create travel_home_page.dart

---
+++ lib/ep1847_travel_app/view/travel_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TravelHomePage extends StatefulWidget {
+  const TravelHomePage({super.key});
+
+  @override
+  State<TravelHomePage> createState() => _TravelHomePageState();
+}
+
+class _TravelHomePageState extends State<TravelHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
