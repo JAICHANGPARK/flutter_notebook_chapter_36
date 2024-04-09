13dfbe7cd3424918c95f5995892067a4da120cdb
JAICHANGPARK <aristojeff@gmail.com>
2024-04-10 08:34:14 +0900

    Create ep1847_main.dart

---
+++ lib/ep1847_travel_app/ep1847_main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(TravelApp());
+}
+
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
