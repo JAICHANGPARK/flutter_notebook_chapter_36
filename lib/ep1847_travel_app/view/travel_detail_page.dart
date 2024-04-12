3a436bfb934a3f78fb0c3ec3243bfd84b6a35370
JAICHANGPARK <aristojeff@gmail.com>
2024-04-12 23:02:47 +0900

    Update travel_detail_page.dart

--- lib/ep1847_travel_app/view/travel_detail_page.dart
+++ lib/ep1847_travel_app/view/travel_detail_page.dart
@@ -25,19 +25,28 @@ class _TravelDetailPageState extends State<TravelDetailPage> {
         children: [
           Expanded(child: Placeholder()),
           Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(24),
+            ),
+            padding: EdgeInsets.all(24),
             child: Row(
               children: [
                 Expanded(
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text("Just check-in"),
                       CircleAvatar(),
                     ],
                   ),
                 ),
-                Expanded(child: Container(child: Center(
-                  child: Text("Select dates"),
-                ),))
+                Expanded(
+                    child: Container(
+                  child: Center(
+                    child: Text("Select dates"),
+                  ),
+                ))
               ],
             ),
           )
